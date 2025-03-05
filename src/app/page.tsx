617b2ae83d676cb97e2bcf734650195d072c17dd
Emmsdev <emmanuelfanhi@gmail.com>
2025-03-05 16:02:12 +0100

    upgrade

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 "use client"
-import Hero from './components/Hero';
-import Timeline from './components/Timeline';
-import Gallery from './components/Gallery';
+// import Hero from './components/Hero';
+// import Timeline from './components/Timeline';
+// import Gallery from './components/Gallery';
 
 export default function Home() {
   return (
